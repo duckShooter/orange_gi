3fb67711a178344fad9913026ea2af3ecf9f4e49
duckShooter <giovanni.n@gmx.us>
2019-04-13 15:40:01 +0200

    add 2 new abstract method to update a product and find all products in a specific category

--- source code/Toy Store/src/main/java/gi/orange/task/services/ProductService.java
+++ source code/Toy Store/src/main/java/gi/orange/task/services/ProductService.java
@@ -5,5 +5,7 @@ import java.util.Set;
 import gi.orange.task.models.Product;
 
 public interface ProductService extends BaseService<Product, Integer> {
-	Set<Product> findAllInCategory();
+	Set<Product> findAllInCategory(String categoryName);
+	Set<Product> findAllInCategory(Integer categoryId);
+	Product updateProduct(Integer id, Product modifiedProduct);
 }
