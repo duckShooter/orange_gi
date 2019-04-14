89e5dbaeb88f4dc03e74546b1e06748840a8d629
duckShooter <giovanni.n@gmx.us>
2019-04-15 01:00:36 +0200

    added user specific DDL & DML statements

--- database/schema.sql
+++ database/schema.sql
@@ -1,3 +1,9 @@
+CREATE TABLE `user` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
 CREATE TABLE `category` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
